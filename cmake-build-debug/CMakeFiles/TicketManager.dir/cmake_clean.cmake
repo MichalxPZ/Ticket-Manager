fb94af9f2fea1a45811d194177d0c516bc388639
MichalxPZ <michauz2001@gmail.com>
2021-11-19 22:29:48 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/TicketManager.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicketManager.dir/main.cpp.obj"
+  "TicketManager.exe"
+  "TicketManager.exe.manifest"
+  "TicketManager.pdb"
+  "libTicketManager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicketManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TicketManager.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TicketManager.
+# This may be replaced when dependencies are built.
