2fa8f75b520b290dc909e4436c2511127459b537
MichalxPZ <michauz2001@gmail.com>
2021-11-24 13:40:47 +0100

    sync

--- cmake-build-debug/CMakeFiles/TicketManager.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/TicketManager.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/TicketManager.dir/main.cpp.obj"
-  "TicketManager.exe"
-  "TicketManager.exe.manifest"
+  "CMakeFiles/TicketManager.dir/main.cpp.o"
+  "TicketManager"
   "TicketManager.pdb"
-  "libTicketManager.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
